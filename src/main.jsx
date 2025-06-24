e3204936dd55ee46bb413848167669a3d9322620
meun8n <victorsantos010111998@gmail.com>
2025-06-24 08:32:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Agendamento from "./components/Agendamento"; // caminho atualizado
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css'; // aplica o estilo global
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Agendamento />
+    <App />
   </React.StrictMode>
 );
