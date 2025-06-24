5ea5ed7c53f6013a63f0ce10f4367b51d334e700
meun8n <victorsantos010111998@gmail.com>
2025-06-24 07:33:00 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Agendamento from "./Agendamento"; // importa o componente completo
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Agendamento />
   </React.StrictMode>
 );
