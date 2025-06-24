a856676f050cd47f748effcbf5f06615070b0400
meun8n <victorsantos010111998@gmail.com>
2025-06-24 08:45:42 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
+import Agendamento from './components/Agendamento';
 
 export default function App() {
-  return (
-    <div>
-      <h2>Agendamento</h2>
-      <p>Selecione uma data e horário:</p>
-      {/* Aqui futuramente colocaremos o calendário e horários */}
-    </div>
-  );
+  return <Agendamento />;
 }
